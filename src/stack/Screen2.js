3ed5fb6d952d39783dc9a0ac0d7e99235035c932
phamtanphat <phatdroid94@gmail.com>
2020-11-17 19:21:35 +0700

    init

---
+++ src/stack/Screen2.js
@@ -0,0 +1,16 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
+
+export default class Screen2 extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontSize: 20}}> Screen 2 </Text>
+        <TouchableOpacity style={{backgroundColor: 'blue', padding: 10}}>
+          <Text style={{fontSize: 20}}>Navigate Screen 3</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
