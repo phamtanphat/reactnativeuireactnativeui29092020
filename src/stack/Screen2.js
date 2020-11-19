2d06d492a613b24cc5d663ad5532723b7d46cacc
phamtanphat <phatdroid94@gmail.com>
2020-11-19 18:37:08 +0700

    update ui

--- src/stack/Screen2.js
+++ src/stack/Screen2.js
@@ -7,14 +7,6 @@ export default class Screen2 extends Component {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text style={{fontSize: 20}}> Screen 2 </Text>
-        <Text style={{fontSize: 20}}>
-          Count : {this.props.route.params.count}
-        </Text>
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('Screen3')}
-          style={{backgroundColor: 'blue', padding: 10}}>
-          <Text style={{fontSize: 20}}>Increase</Text>
-        </TouchableOpacity>
         <TouchableOpacity
           onPress={() => this.props.navigation.goBack()}
           style={{backgroundColor: 'blue', padding: 10}}>
