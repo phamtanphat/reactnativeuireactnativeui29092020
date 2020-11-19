bb9e8d0461dca709f172b29982aef24ef64be2b9
phamtanphat <phatdroid94@gmail.com>
2020-11-19 18:35:59 +0700

    ui

--- src/stack/Screen1.js
+++ src/stack/Screen1.js
@@ -3,17 +3,19 @@ import React, {Component} from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
 
 export default class Screen1 extends Component {
-  componentDidMount() {
-    const {text} = this.props.route.params;
-    alert(text);
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
   }
   render() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{fontSize: 20}}> Screen 1 </Text>
+        <Text style={{fontSize: 20}}> Count {this.state.count} </Text>
         <TouchableOpacity
           onPress={() =>
-            this.props.navigation.navigate('Screen2', {text: 'Hello screen 2'})
+            this.props.navigation.navigate('Screen2', {count: this.state.count})
           }
           style={{backgroundColor: 'red', padding: 10}}>
           <Text style={{fontSize: 20}}>Navigate Screen 2</Text>
--- src/stack/Screen2.js
+++ src/stack/Screen2.js
@@ -3,18 +3,22 @@ import React, {Component} from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
 
 export default class Screen2 extends Component {
-  componentDidMount() {
-    const {text} = this.props.route.params;
-    alert(text);
-  }
   render() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text style={{fontSize: 20}}> Screen 2 </Text>
+        <Text style={{fontSize: 20}}>
+          Count : {this.props.route.params.count}
+        </Text>
         <TouchableOpacity
           onPress={() => this.props.navigation.navigate('Screen3')}
           style={{backgroundColor: 'blue', padding: 10}}>
-          <Text style={{fontSize: 20}}>Navigate Screen 3</Text>
+          <Text style={{fontSize: 20}}>Increase</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => this.props.navigation.goBack()}
+          style={{backgroundColor: 'blue', padding: 10}}>
+          <Text style={{fontSize: 20}}>Go Back</Text>
         </TouchableOpacity>
       </View>
     );
